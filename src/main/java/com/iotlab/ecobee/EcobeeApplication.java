9813a8a1fa767bc795d1edb0c3cebce455ab1dc5
mmorcate <martin.morcate@vertivco.com>
2018-01-09 17:24:00 -0500

    Initial commit

---
+++ src/main/java/com/iotlab/ecobee/EcobeeApplication.java
@@ -0,0 +1,12 @@
+package com.iotlab.ecobee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcobeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcobeeApplication.class, args);
+	}
+}
